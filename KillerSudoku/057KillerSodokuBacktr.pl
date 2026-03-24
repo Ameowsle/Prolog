3f36abfedc49e5eb9df910577721cfa6c6a7424a
Amelia Romana Gasser <165668812+Ameowsle@users.noreply.github.com>
2026-03-24 17:22:55 +0100

    Killersudoku with Backtracking

---
+++ KillerSudoku/057KillerSodokuBacktr.pl
@@ -0,0 +1,106 @@
+
+% THE PUZZLE: Unknown cells are represented as anonymous variables (_).
+puzzle([[_, 6, 4, 8, _, _, 3, _, 7],
+         [_, 5, 8, 2, 3, _, _, 6, _], 
+         [_, _, _, _, _, 4, _, _, _],
+         [2, _, _, _, _, 1, _, _, _],
+         [_, _, _, _, 8, _, _, _, _],
+         [_, _, _, _, _, 5, 9, _, 6],
+         [_, _, 3, 6, _, _, 7, _, 5],
+         [_, _, _, _, _, _, _, 9, 2],
+         [_, 7, _, _, _, _, _, _, 1]]).
+
+cages([
+    cage(3,  [pos(1,1), pos(1,2)]),
+    cage(15, [pos(1,3), pos(2,3), pos(3,3)]),
+    cage(22, [pos(1,6), pos(2,6)]),
+    cage(4,  [pos(1,7), pos(1,8)]),
+    cage(15, [pos(1,9), pos(2,9), pos(3,9)]),
+    cage(25, [pos(2,1), pos(3,1), pos(4,1)]),
+    cage(17, [pos(2,3), pos(2,4), pos(2,5)]),
+    cage(9,  [pos(3,3), pos(3,4)]),
+    cage(8,  [pos(3,6), pos(3,7)]),
+    cage(6,  [pos(4,1), pos(5,1)]),
+    cage(14, [pos(4,2), pos(5,2), pos(5,3)]),
+    cage(17, [pos(4,5), pos(4,6), pos(5,6)]),
+    cage(17, [pos(4,8), pos(4,9), pos(5,9)]),
+    cage(20, [pos(5,4), pos(5,5), pos(6,5)]),
+    cage(12, [pos(5,9), pos(6,9)]),
+    cage(27, [pos(6,1), pos(7,1), pos(8,1)]),
+    cage(6,  [pos(6,3), pos(6,4)]),
+    cage(20, [pos(6,6), pos(7,6), pos(7,5)]),
+    cage(6,  [pos(6,7), pos(7,7)]),
+    cage(10, [pos(7,5), pos(7,4), pos(7,3)]),
+    cage(14, [pos(7,8), pos(7,9)]),
+    cage(8,  [pos(8,2), pos(8,3)]),
+    cage(15, [pos(8,6), pos(8,7)]),
+    cage(13, [pos(9,5), pos(9,6)]),
+    cage(17, [pos(9,8), pos(9,9)])
+]).
+
+sudoku(Solution):-
+    % copy the predefined puzzel into Solution  
+    puzzle(Solution),
+    append(Solution, Vars),
+    fill(Vars, Solution).
+
+fill([], _).
+fill([H|Rest], Solution):-
+    nonvar(H), %checks if H is a Number (Nonvar), returns true if so and goes on (if false the other fill method is called)
+    fill(Rest, Solution).
+
+fill([H|Rest], Solution):-
+    var(H), %checks if H is a _ (var)
+    member(H, [1,2,3,4,5,6,7,8,9]),
+    check_position(Solution),
+    fill(Rest, Solution).
+
+
+check_position(Solution):-
+    rows_all_diff(Solution), % all elements in a row must be distinct
+    cols_all_diff(Solution), % all elements in a col must be distinct
+    blocks_all_diff(Solution),% all 3x3 blocks must be distinct
+    cages(Cages),
+    check_cages(Cages, Solution).
+
+check_cages([],_).
+check_cages([cage(Sum, Pos)| Rest], Solution):-
+    maplist(pos_to_list(Solution), Pos, AsList), %Translates Positions in a List: iterates over Positions list
+    all_diff(AsList),
+    sum_list(AsList, Sum),
+    check_cages(Rest, Solution).
+
+pos_to_list(Matrix, pos(R,C), AsNum):-
+    nth1(R, Matrix, Row),
+    nth1(C, Row, AsNum).
+
+rows_all_diff([]).
+rows_all_diff([FirstRow| Rest]):-
+    all_diff(FirstRow), %checks if the chosen Number fits in the Row 
+    rows_all_diff(Rest).
+
+cols_all_diff(Solution):-
+    numlist(1,9,Indices), %creates list with number 1 to 9
+    maplist(check_with_index(Solution), Indices). %calls function on every index 
+
+check_with_index(Solution, Index):-
+    maplist(nth1(Index), Solution, Col), %takes the element at Index from every Row and stores it in Col
+    all_diff(Col).  %checks if the chosen Number fits in the Col 
+
+
+blocks([], [], []).
+blocks([V1,V2,V3|Rest1], [V4,V5,V6|Rest2], [V7,V8,V9|Rest3]) :-
+    all_diff([V1,V2,V3,V4,V5,V6,V7,V8,V9]),
+    blocks(Rest1, Rest2, Rest3).
+
+blocks_all_diff([]).
+blocks_all_diff([R1, R2, R3 | Rest]):-
+    blocks(R1, R2, R3),
+    blocks_all_diff(Rest).
+
+all_diff(List) :-
+    include(nonvar, List, OnlyNum),  %only keep the numbers (remove all _)
+    sort(OnlyNum, Sorted), % sort removes duplicates
+    %check if list is the same after the sort
+    length(OnlyNum, N), 
+    length(Sorted, N).
