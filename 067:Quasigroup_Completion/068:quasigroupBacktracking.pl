66c1bfd6656c4e6883904ad5df31b36e429d9107
Amelia Romana Gasser <165668812+Ameowsle@users.noreply.github.com>
2026-04-05 14:29:42 +0200

    commentes

--- 067:Quasigroup_Completion/068:quasigroupBacktracking.pl
+++ 067:Quasigroup_Completion/068:quasigroupBacktracking.pl
@@ -10,7 +10,7 @@ quasigroup(Solution):-
     partially_quasigroup(Solution), % stores partially pre-filled quasigroup in Solution
     length(Solution, M),
     numlist(1, M, Domain),
-    maplist(assign_and_check_row(Domain), Solution),  % Zeile für Zeile prüfen
+    maplist(assign_and_check_row(Domain), Solution),  % Check line by line 
     numlist(1, M, Indices),
     maplist(check_col(Solution), Indices).
 
@@ -19,12 +19,12 @@ check_col(Solution, Index):-
     all_diff(Col).
 
 assign_and_check_row(Domain, Row) :-
-    maplist(assign_cell(Domain), Row),  % Zelle für Zelle belegen
-    all_diff(Row).                      % Zeile sofort prüfen
+    maplist(assign_cell(Domain), Row),  % assign num from domain for every row
+    all_diff(Row). %check row immediately
 
-assign_cell(_, Cell) :- nonvar(Cell).  % bereits fixiert
+assign_cell(_, Cell) :- nonvar(Cell).  
 assign_cell(Domain, Cell) :- 
-    member(Cell, Domain).              % offen: Wert aus Domain wählen
+    member(Cell, Domain).              
 
 all_diff(List) :-
     sort(List, Sorted),
