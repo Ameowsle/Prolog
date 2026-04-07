6586e8ec4bf6c3edb0a97ec008942926a2fe59a6
Amelia Romana Gasser <165668812+Ameowsle@users.noreply.github.com>
2026-04-07 17:12:06 +0200

    New puzzle

--- KillerSudoku/057:GeneralisationCLP.pl
+++ KillerSudoku/057:GeneralisationCLP.pl
@@ -2,23 +2,48 @@
 
 % THE PUZZLE: Unknown cells are represented as anonymous variables (_).
 puzzle([
-    [_, _, _, _, _, _, _, _, _],
-    [_, _, _, _, _, _, _, _, _],
-    [_, _, _, _, _, _, _, _, _],
-    [_, _, _, _, _, _, _, _, _],
-    [_, _, _, _, _, _, _, _, _],
-    [_, _, _, _, _, _, _, _, _],
+    [2, _, 5, _, _, _, _, _, _],
+    [_, 6, _, _, _, _, _, _, _],
+    [_, 9, _, _, _, _, _, _, _],
+    [_, _, _, 2, _, _, _, _, _],
+    [1, _, _, _, _, _, _, _, _],
+    [9, 7, _, _, _, _, _, _, _],
     [_, _, _, _, _, _, _, _, _],
     [_, _, _, _, _, _, _, _, _],
     [_, _, _, _, _, _, _, _, _]
 ]).
 
 cages([
-    cage(3,  [pos(1,1), pos(1,2)]),
-    cage(15, [pos(1,3), pos(2,3), pos(3,3)]),
-    cage(22, [pos(1,6), pos(2,6)])
+    cage(3,  [pos(1,1), pos(1,2)]),             
+    cage(15, [pos(1,3), pos(1,4), pos(1,5)]),
+    cage(22, [pos(1,6), pos(2,6), pos(2,5), pos(3,5)]), 
+    cage(4,  [pos(1,7), pos(2,7)]),             
+    cage(16, [pos(1,8), pos(2,8)]),                        
+    cage(15, [pos(1,9), pos(2,9), pos(3,9), pos(4,9)]),   
+    cage(25, [pos(2,1), pos(3,1), pos(3,2), pos(2,2)]),  
+    cage(17, [pos(2,3), pos(2,4)]),             
+    cage(9,  [pos(3,3), pos(3,4), pos(4,4)]),             
+    cage(8,  [pos(3,6), pos(4,6), pos(5,6)]),            
+    cage(20, [pos(3,8), pos(3,7), pos(4,7)]),   
+    cage(6,  [pos(4,1), pos(5,1)]), 
+    cage(14, [pos(4,2), pos(4,3)]),
+    cage(17, [pos(4,5), pos(5,5), pos(6,5)]),
+    cage(17, [pos(5,7), pos(5,8), pos(4,8)]),
+    cage(13, [pos(5,2), pos(6,2), pos(5,3)]),
+    cage(20, [pos(5,4), pos(6,4), pos(7,4)]), 
+    cage(20, [pos(6,6), pos(7,6), pos(7,7)]),
+    cage(12, [pos(5,9), pos(6,9)]),   
+    cage(27, [pos(6,1), pos(7,1), pos(8,1), pos(9,1)]),  
+    cage(6,  [pos(6,3), pos(7,2), pos(7,3)]),             
+    cage(6,  [pos(6,7), pos(6,8)]), 
+    cage(10, [pos(7,5), pos(8,5), pos(8,4), pos(9,4)]),   
+    cage(14, [pos(7,8), pos(7,9), pos(8,8), pos(8,9)]),   
+    cage(8,  [pos(8,2), pos(9,2)]),           
+    cage(16, [pos(8,3), pos(9,3)]), 
+    cage(15, [pos(8,6), pos(8,7)]),
+    cage(13, [pos(9,5), pos(9,6), pos(9,7)]),
+    cage(17, [pos(9,8), pos(9,9)]) 
 ]).
-
 sudoku(Solution):-
     % copy the predefined puzzel into Solution  
     puzzle(Solution),
